fdd0d99b87244254dbc38d1e37a9384a9149c73f
Vivek Patidar <mac@vivek.local>
2023-12-07 10:32:33 +0530

    feat: add cookie authentication

--- middlewares/authentication.middleware.js
+++ middlewares/authentication.middleware.js
@@ -1,46 +1,61 @@
-const JWT = require('jsonwebtoken');
 const { commonErrorHandler } = require('../helpers/error-handler.helper');
+const redisHelper = require('../helpers/redis.helper');
 
-const verifyAccessToken = (req, res, next) => {
+const verifyAccessToken = async (req, res, next) => {
+  // if  cookies are not present, send error
   if (!req.cookies) {
     return commonErrorHandler(req, res, 'unauthorized', 401);
   }
+  // get the access token from the cookie
   const accessToken = req.cookies.accessToken;
 
+  // if access token not present, send error
   if (!accessToken) {
     return commonErrorHandler(req, res, 'unauthorized', 401);
   }
 
-  JWT.verify(
-    accessToken,
-    process.env.JWT_ACCESS_TOKEN_SECRET_KEY,
-    (err, payload) => {
-      if (err) {
-        return commonErrorHandler(req, res, 'unauthorized', 401);
-      }
-      req.user = payload;
-      next();
-    },
-  );
-};
+  // get the value of the access token key from redis db
+  const userId = await redisHelper.getAsync(accessToken);
 
-const verifyRefreshToken = (req, res, next) => {
-  const refreshToken = req.cookies.refreshToken;
-  if (!refreshToken) {
-    return commonErrorHandler(req, res, 'refresh token not found', 400);
+  // if no value found, the access token is not present in redis, send 401 (unauthorized)
+  if (!userId) {
+    return commonErrorHandler(req, res, 'unauthorized', 401);
   }
 
-  JWT.verify(
-    refreshToken,
-    process.env.JWT_REFRESH_TOKEN_SECRET_KEY,
-    (err, payload) => {
-      if (err) {
-        return commonErrorHandler(req, res, 'unauthorized', 401);
-      }
-      req.user = payload;
-      next();
-    },
-  );
+  // set the user id to request object
+  req.user = { id: userId };
+
+  // call next middleware
+  next();
+};
+
+const verifyRefreshToken = async (req, res, next) => {
+  // if  cookies are not present, send error
+  if (!req.cookies) {
+    return commonErrorHandler(req, res, 'unauthorized', 401);
+  }
+
+  // get the refresh token from the cookie
+  const refreshToken = req.cookies.refreshToken;
+
+  // if refresh token not present, send error
+  if (!refreshToken) {
+    return commonErrorHandler(req, res, 'unauthorized', 401);
+  }
+
+  // get the value of the refresh token key from redis db
+  const userId = await redisHelper.getAsync(refreshToken);
+
+  // if no value found, the refresh token is not present in redis, send 401 (unauthorized)
+  if (!userId) {
+    return commonErrorHandler(req, res, 'unauthorized', 401);
+  }
+
+  // set the user id to request object
+  req.user = { id: userId };
+
+  // call next middleware
+  next();
 };
 
 module.exports = {
--- services/admin.service.js
+++ services/admin.service.js
@@ -1,6 +1,7 @@
 const User = require('../models/User.model');
 const bcrypt = require('bcrypt');
 
+const redisHelper = require('../helpers/redis.helper');
 const {
   signAccessToken,
   signRefreshToken,
@@ -8,26 +9,54 @@ const {
 
 const loginAdmin = async (payload) => {
   try {
+    // get the email and password from the payload
     const { email, password } = payload;
+
+    // find if the user exists with the email
     const user = await User.findOne({ email: email });
+
+    // if no user exists, send 404 (not found)
     if (!user) {
       const error = new Error('user not registered');
       error.statusCode = 404;
       throw error;
     }
 
+    // check if the credentials matches
     const isMatch = await bcrypt.compare(password, user.password);
+
+    // give 401 (unauthorized), if credentials not match
     if (!isMatch) {
       const error = new Error('email/password is invalid');
       error.statusCode = 401;
       throw error;
     }
+
+    // get the user id from the user object for storing it in the token value
     const { _id: userId } = user;
 
+    // get the access and refresh token by setting user id in the value
     const accessToken = await signAccessToken(userId);
     const refreshToken = await signRefreshToken(userId);
 
+    // return access token and refresh token to the controller
     return { accessToken, refreshToken };
+  } catch (error) {
+    throw error;
+  }
+};
+
+const logoutAdmin = async (payload) => {
+  try {
+    // get the access token and refresh token from the payload
+    const { accessToken, refreshToken } = payload;
+
+    // delete tokens from redis
+    await redisHelper.deleteAsync(accessToken);
+    await redisHelper.deleteAsync(refreshToken);
+
+    // return success log out message to the controller
+    return 'Logged out successfully';
   } catch (error) {
     throw error;
   }
@@ -35,9 +64,13 @@ const loginAdmin = async (payload) => {
 
 const registerAdmin = async (payload) => {
   try {
+    // get the user details from the payload
     const { firstName, lastName, email, password, role } = payload;
 
+    // check if user already exists with the same email address
     const existingAdmin = await User.findOne({ email: email.toLowerCase() });
+
+    // if user exists, send 409 (conflict)
     if (existingAdmin) {
       const error = new Error('email already registered');
       error.statusCode = 409;
@@ -47,6 +80,7 @@ const registerAdmin = async (payload) => {
     // hashing the password using bcrypt
     const hashedPassword = await bcrypt.hash(password, 10);
 
+    // create the user payload for admin
     const admin = new User({
       firstName,
       lastName,
@@ -55,9 +89,26 @@ const registerAdmin = async (payload) => {
       role: role,
     });
 
+    // save the user in the database
     const savedUser = await admin.save();
 
+    // return the saved user
     return savedUser;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const refreshAccessToken = async (payload) => {
+  try {
+    // get the access token and refresh token from the payload
+    const { accessToken, refreshToken } = payload;
+
+    // delete tokens from redis
+    if (accessToken) await redisHelper.deleteAsync(accessToken);
+    await redisHelper.deleteAsync(refreshToken);
+
+    return;
   } catch (error) {
     throw error;
   }
@@ -65,5 +116,7 @@ const registerAdmin = async (payload) => {
 
 module.exports = {
   loginAdmin,
+  logoutAdmin,
   registerAdmin,
+  refreshAccessToken,
 };
