ba086d4fa04d732642b55f16d38a6cfb5e05b76a
Vivek Patidar <mac@vivek.local>
2023-12-11 15:19:53 +0530

    feat: add genres in create genre script

--- scripts/createGenres.js
+++ scripts/createGenres.js
@@ -16,6 +16,44 @@ const genresData = [
     name: 'comedy',
     description: 'Movies intended to make the audience laugh.',
   },
+  {
+    name: 'sci-fi',
+    description:
+      'Movies set in futuristic or speculative worlds with advanced technology.',
+  },
+  {
+    name: 'horror',
+    description: 'Movies designed to evoke fear and suspense in the audience.',
+  },
+  {
+    name: 'romance',
+    description:
+      'Movies centered around romantic relationships and love stories.',
+  },
+  {
+    name: 'documentary',
+    description:
+      'Films that provide a factual and informative account of real events.',
+  },
+  {
+    name: 'fantasy',
+    description:
+      'Movies featuring magical or fantastical elements often set in imaginary worlds.',
+  },
+  {
+    name: 'thriller',
+    description: 'Movies characterized by intense suspense and excitement.',
+  },
+  {
+    name: 'animation',
+    description:
+      'Movies created through animation techniques, often targeting both children and adults.',
+  },
+  {
+    name: 'mystery',
+    description:
+      'Movies that involve solving a mysterious event or uncovering hidden truths.',
+  },
 ];
 
 const addGenres = async () => {
